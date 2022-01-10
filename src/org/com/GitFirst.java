6983a39fe74f097d7dac4e2d5557fb90f231e96e
sarojinisamyrajulu <sarogreens@gmail.com>
2022-01-10 12:35:01 +0530

    Success

---
+++ src/org/com/GitFirst.java
@@ -0,0 +1,19 @@
+package org.com;
+
+public class GitFirst {
+	
+
+		public void empId() {
+			System.out.println("empId: 12");
+		}
+		 
+		public void empName() {
+			System.out.println("empName: saro");
+
+		}
+		public static void main(String[] args) {
+			GitFirst g = new GitFirst();
+			g.empId();
+			g.empName();
+		}
+}
